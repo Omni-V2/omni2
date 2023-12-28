34e47406ed3d49ca97e39a3b55cefcbadc344054
Aymenlahrech <aymenlahrech00@gmail.com>
2023-12-28 21:21:05 +0100

    css/productDetails

--- app/comps/SingleProd.tsx
+++ app/comps/SingleProd.tsx
@@ -1,13 +1,18 @@
 import Addtochart from "../comps/Addtochart"
 import ProdImage from "../comps/ProdImage"
+import Link from 'next/link';
+
 export default function Single({data}) {
   console.log(data);
-  
+
     return (
       <div className="flex flex-col gap-2">
       <div className="relative">
+
+      <Link href={{ pathname: '/products', query: { data: JSON.stringify(data) } }}>
           <ProdImage img={data.imageUrl} sales={data.sales}/>
           <Addtochart />
+          </Link>
       </div>
       <div className="flex flex-col gap-2">
           <h2>{data.productName}</h2>
