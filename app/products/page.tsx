6b699bb27cdd0c4d680d29c71a88c8a1f44bcde7
houssemboulaares <houssem.boulaares.123@gmail.com>
2023-12-28 13:10:14 +0100

    home

---
+++ app/products/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+
+export default function product() {
+  return (
+<div>
+ 
+       
+        <Link href={'/'}> <h1>  hello product</h1></Link>
+ 
+ 
+</div>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,26 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+
   theme: {
+    colors:{
+      transparent: 'transparent',
+      current: 'currentColor',
+      'back':'#a39ea4',
+      'inputs':'#f5f5f5',
+      'red':'#db4444',
+      'black':'#000000',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'gray':'#f5f5f5',
+      'gray-300':'#cbd5e1',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
