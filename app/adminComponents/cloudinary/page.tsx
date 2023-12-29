6d2556c76b61ba953e6fc3106c51bd448ca6935f
Aymenlahrech <aymenlahrech00@gmail.com>
2023-12-29 22:27:25 +0100

    page cloudinary with typescript-page productlist and add product css and typescript

--- app/adminComponents/cloudinary/page.tsx
+++ app/adminComponents/cloudinary/page.tsx
@@ -1,36 +1,47 @@
-import { Request, Response } from 'express';
-import fetch from 'node-fetch';
-import FormData from 'form-data';
 
-const cloudinaryUpload = async (req: Request, res: Response) => {
+"use client"
+import React, { useState, ChangeEvent } from 'react';
+import { MdOutlineAddPhotoAlternate } from 'react-icons/md';
+
+
+
+
+function Cloudinary({ setImg }: any) {
   const cloudName = 'dcq9dwrsb';
   const presetName = 'l4ng65bl';
 
-  try {
-    if (!req.files || Object.keys(req.files).length === 0) {
-      return res.status(400).send('No files were uploaded.');
+  const handleUpload = async (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if (!file) {
+      return;
     }
 
-    const file = req.files.file as UploadedFile; 
-
     const formData = new FormData();
-    formData.append('file', file.data);
+    formData.append('file', file);
     formData.append('upload_preset', presetName);
 
-    const response = await fetch(
-      `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
-      {
+    try {
+      const response = await fetch(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, {
         method: 'POST',
         body: formData,
-      }
-    );
+      });
 
-    const data = await response.json();
-    res.status(200).json({ secureUrl: data.secure_url });
-  } catch (error) {
-    console.error('Error uploading image: ', error);
-    res.status(500).json({ error: 'Error uploading image' });
-  }
-};
+      const data = await response.json();
+      setImg(data.secure_url);
+    } catch (error) {
+      console.error('Error uploading image: ', error);
+    }
+  };
 
-export default cloudinaryUpload;
+  return (
+    <div className='cloudinary'>
+      <label>
+        <MdOutlineAddPhotoAlternate />
+        <input type="file" onChange={handleUpload} />
+      </label>
+    </div>
+  );
+}
+
+export default Cloudinary;
