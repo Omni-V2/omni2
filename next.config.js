1d546acb7ec869fd78ad6f480ba3639a6ca078c3
Rahma090 <rahma.aliani@gmail.com>
2023-12-31 19:50:06 +0100

    images URLs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = { images: {
-    domains: ['s3-alpha-sig.figma.com','example.com', 'anotherdomain.com', 'third-domain.com','https://images-ext-1.discordapp.net/external/cLI-5nniI7SCWx0crvR0wvNuG6rvpUSJQATsEtN_tDc/https/i.ibb.co/6W1WTg3/moving-logo4.gif?width=767&height=545','i.ibb.co'],
+    domains: ['s3-alpha-sig.figma.com','example.com', 'anotherdomain.com','static.nike.com','third-domain.com','https://images-ext-1.discordapp.net/external/cLI-5nniI7SCWx0crvR0wvNuG6rvpUSJQATsEtN_tDc/https/i.ibb.co/6W1WTg3/moving-logo4.gif?width=767&height=545','i.ibb.co'],
   },}
 
 module.exports = nextConfig
